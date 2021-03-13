4c6de7bfeb34728b8966e8044ad3b8e7764ee8fb
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-13 19:43:01 +0100

    subscription html and css

--- subscription.php
+++ subscription.php
@@ -1,5 +1,24 @@
 <?php include "partials/header.php"; ?>
-
+<section class="subscription">
+    <article class="container">
+    <div>
+        <h2>Subscription from $15/month</h2>
+        <h4>Start a free trial now</h4>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel recusandae in repellendus perferendis accusamus cumque ad eum facere laboriosam similique ducimus nihil, quibusdam expedita fuga alias ea ab inventore harum.</p>
+        <button>Start free trial</button>
+    </div>
+    <div>
+        <ul>
+            <li>Play any track</li>
+            <li>Listen offline</li>
+            <li>No add interruptions</li>
+            <li>Unlimited skips</li>
+            <li>High quality</li>
+            <li>Shuffle play</li>
+        </ul>
+    </div>
+    </article>
+</section>
 <section class="concept container">
         <article>
             <h2>Our Concept & artists</h2>
