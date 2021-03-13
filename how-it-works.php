25cf148dbc017617256b1c0b9cf781668413a5b9
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-13 18:55:02 +0100

    hivHeader html and css

--- how-it-works.php
+++ how-it-works.php
@@ -1,5 +1,25 @@
 <?php include "partials/header.php"; ?>
 
-    <h1 style="font-size: 9rem; text-align:center;" >How it works</h1>
+    <!--hivHeader-->
+    <section class="hivHeader">
+        <h2 class="container">How it works</h2>
+        <article class="container">
+            <div>
+                <i class="fas fa-user-circle"></i>
+                <h3>Create an account</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem dicta quos numquam laboriosam doloremque omnis similique necessitatibus officia dolorum nesciunt!</p>
+
+            </div>
+            <div>
+                <i class="fas fa-drum"></i>
+                <h3>Create an account</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem dicta quos numquam laboriosam doloremque omnis similique necessitatibus officia dolorum nesciunt!</p>
+            </div>
+            <div><i class="fas fa-cloud-download-alt"></i>
+                <h3>Create an account</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem dicta quos numquam laboriosam doloremque omnis similique necessitatibus officia dolorum nesciunt!</p>
+            </div>
+        </article>
+    </section>
 
 <?php include "partials/footer.php"; ?>
