c0f3d8c25ddc323a88c57f26a43be61765f2922a
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-13 19:19:53 +0100

    Concept html and css

--- subscription.php
+++ subscription.php
@@ -1,5 +1,30 @@
 <?php include "partials/header.php"; ?>
 
-    <h1 style="font-size: 9rem; text-align:center;" >Subscriptions</h1>
+<section class="concept container">
+        <article>
+            <h2>Our Concept & artists</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ullam rem, doloremque voluptas dolorum in perferendis qui. Nam doloribus quia aliquam distinctio corporis! Quae molestiae distinctio tempore nam natus sequi?
+            </p>
+        </article>
+        <article>
+            <div>
+                <img src="img/subscription1.jpg" alt="">
+                <h3>No ad interruptions</h3>
+            </div>
+            <div>
+                <img src="img/subscription2.jpg" alt="">
+                <h3>High quality</h3>
+            </div>
+            <div>
+                <img src="img/subscription3.jpg" alt="">
+                <h3>Listen offline</h3>
+            </div>
+            <div>
+                <img src="img/subscription4.jpg" alt="">
+                <h3>Download music</h3>
+            </div>
+        </article>
+    </section>
 
 <?php include "partials/footer.php"; ?>
