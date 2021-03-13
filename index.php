f7ea8b27284682684d5378d09c28ffaca4006972
urosmarkovic28 <71453832+urosmarkovic28@users.noreply.github.com>
2021-03-13 16:37:21 +0100

    header html and css

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php include "partials/header.php"; ?>
 
-    <h1 style="font-size: 9rem; text-align:center;" >Home</h1>
+    <!--header-->
+    <header class="mainHeader">
+        <div>
+            <h1><span>Music</span> for everyone</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam excepturi dolor minus dignissimos quis? Optio nulla et animi distinctio unde consequuntur, eius magni eaque quaerat nesciunt repudiandae, exercitationem officia est!</p>
+            <button>Start free trial</button>
+        </div>
+        <div>
+            <img src="img/header.png" alt="">
+        </div>
+    </header>
 
 <?php include "partials/footer.php"; ?>
